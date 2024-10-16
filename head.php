78161f749f754d6f4cb502f31026086acede3571
kascik04 <2251010048khang@ou.edu.vn>
2024-10-16 14:09:49 +0700

    set permission cho t

--- head.php
+++ head.php
@@ -33,5 +33,6 @@
         include('../admin/connect.php');?>
 
 <p>Danh pro</p>
+<p>cho Danh</p>
 
 <body class="dashboard" data-aos-easing="ease-out-quart" data-aos-duration="500" data-aos-delay="0">
